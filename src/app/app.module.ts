105f4e385f0a3a14cb5c6108247c55412b0e5bef
xd20110642 <790057064@qq.com>
2018-01-30 11:32:32 +0800

    '修复已知bug和新增收索功能'

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,17 +4,26 @@ import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
 import { AmapComponent } from './amap/amap.component';
+import {FormsModule, ReactiveFormsModule} from '@angular/forms';
+import {RouterModule, Routes} from '@angular/router';
 
-
+const router: Routes = [
+  {path: '', component: AmapComponent},
+  // {path:}
+]
 @NgModule({
   declarations: [
     AppComponent,
     AmapComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    FormsModule,
+    ReactiveFormsModule,
+    RouterModule.forRoot(router)
   ],
   providers: [],
   bootstrap: [AppComponent]
 })
+
 export class AppModule { }
